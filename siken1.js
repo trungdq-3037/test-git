df263eab9e6bf3d035c9894d0c2009da44e980fc
trungdq-3037 <do.quang.trung@sun-asterisk.com>
2022-11-21 16:18:05 +0900

    resolve conflict siken 1

--- siken1.js
+++ siken1.js
@@ -2,13 +2,8 @@ var a = 1
 var b = 2
 
 
-<<<<<<< HEAD
 console.log(123123123123123123)
 console.log(123123123123123123)
-=======
-console.log(9999999999)
-console.log(9999999999999999)
->>>>>>> main
 
 
 
