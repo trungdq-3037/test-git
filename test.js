f5b289e80f8dec30de4789a1a11ac1733a68626b
trungdq-3037 <do.quang.trung@sun-asterisk.com>
2022-11-21 15:57:09 +0900

    create msg

---
+++ test.js
@@ -0,0 +1,32 @@
+var a=3
+var b =1
+var abc
+
+
+
+
+
+console.log(abc)
+console.log(abc)
+
+
+
+
+
+var d=  a+b +1
+var g = a+a
+var c=32
+
+
+
+
+
+
+console.log(321)
+console.log(a+b)
+
+
+
+
+
+console.log(a+b+c+d+3)
