3b28468dcadd9f85ba8b8f5ff266f4aca3b48332
trungdq-3037 <do.quang.trung@sun-asterisk.com>
2022-11-21 13:34:12 +0900

    resolve conflict

--- a.js
+++ a.js
@@ -1,23 +1,3 @@
-<<<<<<< HEAD
-var a = 1
-var b = 2
-var ab = 3
-var cd = 4
-
-
-var g = 12
-var f = 22
-
-
-
-
-
-
-
-g = a + b + c
-
-console.log(g)
-=======
 var a = 1 
 var b = 2
 var conflict_1 = 32
@@ -25,4 +5,3 @@ var conflict_2 = 42
 
 var num=2
 console.log(conflict_1+ conflict_2)
->>>>>>> main
